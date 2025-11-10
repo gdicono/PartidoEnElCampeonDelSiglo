518d04fd3d320c39cba70f0b801ef073fb13c012
Gonzalo Di Cono <gonzalo.dicono@correo.ucu.edu.uy>
2025-11-10 09:47:25 -0300

    Creación de clase DualPrint para que printee en consola y tambien en un .xml

---
+++ src/main/java/org/example/DualPrint.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.PrintStream;
+import java.io.OutputStream;
+
+
+public class DualPrint extends PrintStream { // su objetivo es enviar la misma información a dos lugares a la vez: la consola y un archivo.
+
+    private final PrintStream consoleOut; // referencia al printStream original de la consola (System.out)
+
+    public DualPrint(OutputStream fileOut, PrintStream consoleOut) { // recibe dos flujos de salida. fileOut (hacia el xml) y consoleOut (hacia la consola)
+        super(fileOut, true); // true = auto-flush. llama al constructor de PrintStream con auto-flush activado (true). esto hace que se vacíe el buffer automáticamente después de cada println()
+        this.consoleOut = consoleOut;
+    }
+
+    @Override
+    public void write(byte[] buf, int off, int len) {
+        try {
+
+            super.write(buf, off, len); // escribe los datos en el archivo indicado por fileOut.
+            super.flush(); // asegura que el texto se grabe de inmediato
+
+            consoleOut.write(buf, off, len); // escribimos en la consola
+            consoleOut.flush(); // fuerza la salida en la terminal
+
+        } catch (Exception e) {
+            setError();
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,23 @@
 package org.example;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
 
-    Menu menu = new Menu(); // creamos objeto menu
-    menu.showMenu(); // inicializamos el metodo de menu
+        try {
+            PrintStream dualOut = new DualPrint(new FileOutputStream("resultado.xml"), System.out);
+            System.setOut(dualOut);  // redirige la salida por default (System.out) hacia dualOut.
+            System.setErr(dualOut); // tambien con los errores
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(); // lanza excepcion si hay algun error
+        }
+
+        Menu menu = new Menu(); // creamos objeto menu
+
+        menu.showMenu(); // inicializamos el metodo de menu
 
     }
 }
